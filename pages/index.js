e7dba9a1fab084b3aed9ae39f140a5c55cf17cfd
unknown <hassanad94@gmail.com>
2022-08-28 20:08:52 +0200

    refactor code, lose many grid and optimaze image tags

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,7 @@ export default function Home( { defaultData } ) {
 
   const { hero, title_1 } = defaultData[0];
 
+  const heroImage = urlForImage(hero).url();
 
   return (
 
@@ -31,17 +32,19 @@ export default function Home( { defaultData } ) {
 
     <div className="hero-container fw">
 
-      <img src={urlForImage(hero)} alt="Hero" className='hero' />
+
+      <Image loader={() => heroImage} unoptimized={true} src={heroImage} layout='fill' />
+      {/* <Image layout='fill' src={heroImage.url()} decoding="sync" alt="Hero" className='hero' /> */}
      
     </div>  
     
-    <div className="section title-desc">
+    <div className="section title-desc intro ">
 
       <PortableText className='teszt'
         value={title_1}
       />
 
-      <div className='buttonWithArrow button grid-s-3'>
+      <div className='buttonWithArrow button'>
 
         Bővebben
 
@@ -49,47 +52,58 @@ export default function Home( { defaultData } ) {
      
     </div>
 
-    <div className='section grid-center col2 interested'>
+    <div className='section interested'>
 
       <h2>Ha szeretnél</h2>
+      
+      <div className='image-card-container'>
 
-      <div className='image-card-container'> 
-        <img src='/img/reason-1.png' alt="Indok miért csatlakozz" />
-        <p>Fejlődni aszfalton</p>
+        <div className='image-card'> 
+            <Image width={200} height="266" src='/img/reason-1.png' alt="Indok miért csatlakozz" />
+          <p>Fejlődni aszfalton</p>
+        </div>
+
+        <div className='image-card'> 
+            <Image width={200} height="266" src='/img/reason-2.png' alt="Indok miért csatlakozz" />
+          <p>Fejlődni terepen</p>
+        </div>
+
+        <div className='image-card'> 
+            <Image width={200} height="266" src='/img/reason-3.png' alt="Indok miért csatlakozz" />
+          <p>Elkezdeni futni</p>
+        </div>
+
+        <div className='image-card'> 
+            <Image width={200} height="266" src='/img/reason-4.png' alt="Indok miért csatlakozz" />
+          <p>Csapathoz tartozni</p>
+        </div>
+
       </div>
 
-      <div className='image-card-container'> 
-        <img src='/img/reason-2.png' alt="Indok miért csatlakozz" />
-        <p>Fejlődni terepen</p>
+      <div className='button-container center'>
+        
+        <div className="button btn center center-grid" >
+    
+          Érdekel!
+    
+        </div>
+
       </div>
 
-      <div className='image-card-container'> 
-        <img src='/img/reason-3.png' alt="Indok miért csatlakozz" />
-        <p>Elkezdeni futni</p>
-      </div>
-
-      <div className='image-card-container'> 
-        <img src='/img/reason-4.png' alt="Indok miért csatlakozz" />
-        <p>Csapathoz tartozni</p>
-      </div>
-
-
-      <div className={ "button btn center center-grid" }>
-  
-        Érdekel!
-  
-      </div>
 
 
     </div>
 
-    <div className='section grid-center col3 expect'>
+    <div className='section grid-center expect'>
 
       <h2 className='center'>Mire számíthatsz nálunk</h2>
 
-      <img src='/img/mountain-dec.png' alt="hegy dekoráció" className='mountain-dec' />
+      <div className='mountain-dec-container'>
+        <Image layout='fill' src='/img/mountain-dec.png' alt="hegy dekoráció" className='mountain-dec' />
+      </div>
 
-      <div className='mountain-items'>
+
+      <div className='mountain-items flex column'>
 
         <p className='item'>egy szuper csapat </p>
         <p className='item'>egyéni edzésterv </p>
