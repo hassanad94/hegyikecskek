69eeca045727e6d67aad64d19a6638912ad88aa3
unknown <hassanad94@gmail.com>
2022-09-02 11:43:51 +0200

    navbar fixing

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { client } from '/lib/client';
 import { useEffect, useState } from 'react';
-//import useSWR from 'swr';
 
 const Footer = () => {
 
