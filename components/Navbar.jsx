7d1cdae712a5574f92561ebd9ee306522ec87f0f
unknown <hassanad94@gmail.com>
2022-09-16 00:24:40 +0200

    navbar Changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,159 +1,127 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import Image from "next/image";
 
-
 const Navbar = () => {
-
   const simpleLinks = [
     { name: "Főoldal", link: "/" },
     { name: "Rólunk", link: "/rolunk" },
-    { name: "Edzéstervezés", link: "/edzestervezes", },
-    { name: "Egyesület", link: "/egyesulet", },
+    { name: "Edzéstervezés", link: "/edzestervezes" },
+    { name: "Egyesület", link: "/egyesulet" },
     { name: "Esemenyek", link: "/esemenyek" },
     { name: "Eredmények", link: "/eredmenyek" },
     { name: "Galéria", link: "/galeria" },
   ];
-  
+
   const highlightedLinks = [
     { name: "Edzéstervezés", link: "/edzestervezes" },
     { name: "Tagság", link: "/tagsag" },
     { name: "Kapcsolat", link: "/kapcsolat" },
-  
-  ]
+  ];
 
-  const [activeBar, setActiveBar] = useState( false );
+  const [activeBar, setActiveBar] = useState(false);
 
   const router = useRouter();
 
   const [activeMenuPath, setActiveMenuPath] = useState(router.pathname);
 
-  
   useEffect(() => {
-
-    const allLink = document.querySelectorAll( `.navbar .menu-content .link-item` );
-
-    for (let i = 0; i < allLink.length; i++) {
-      
-      const link = allLink[i];
-      
-      if( link.attributes.href.value === router.pathname ){
-
-        link.closest( "li" ).classList.add('active');
-        continue;
-
-      }
-      
-      link.closest( "li" ).classList.remove('active');
-      
-    }
-
-    return (
-
-      setActiveBar( false )
-
+    const allLink = document.querySelectorAll(
+      `.navbar .menu-content .link-item`
     );
 
-   
-  },[router.pathname]);
+    for (let i = 0; i < allLink.length; i++) {
+      const link = allLink[i];
 
+      if (link.attributes.href.value === router.pathname) {
+        link.closest("li").classList.add("active");
+        continue;
+      }
+
+      link.closest("li").classList.remove("active");
+    }
+
+    return setActiveBar(false);
+  }, [router.pathname]);
 
   return (
-
-    <div className={ "navbar" + ( activeBar ? " open" : "" )  } >
-
-      <div className='wrapper'>
-
-        <div className="hamburger-wrapper mobile" onClick={() => setActiveBar(!activeBar) }>
-
+    <div className={"navbar" + (activeBar ? " open" : "")}>
+      <div className="wrapper">
+        <div
+          className="hamburger-wrapper mobile"
+          onClick={() => setActiveBar(!activeBar)}
+        >
           <div className="hamburger"></div>
           <div className="hamburger"></div>
           <div className="hamburger"></div>
-
         </div>
 
         <div className="logo-container">
-
-          <Image layout="fill" src="/logo.png" alt="Logo" className='logo' />
-
+          <Image layout="fill" src="/logo.png" alt="Logo" className="logo" />
         </div>
 
         <div className="menu-content desktop">
-
           <ul className="menu-items">
-
             <div className="flex">
-
               {simpleLinks.map((item, i) => (
-                <li className="" key={`${i * 4}`}  >
-                  <Link href={item.link}>
-                    <a className="link-item" >{item.name}</a>
-                  </Link>
-                </li>
-              ))}
-
-            </div>
-
-            <div className="separeted">
-
-              {highlightedLinks.map((item, i) => (
-                <li className="" key={`${i * 4}`} >
+                <li className="" key={`${i * 4}`}>
                   <Link href={item.link}>
                     <a className="link-item">{item.name}</a>
                   </Link>
                 </li>
               ))}
-
             </div>
 
+            <div className="separeted">
+              {highlightedLinks.map((item, i) => (
+                <li className="" key={`${i * 4}`}>
+                  <Link href={item.link}>
+                    <a className="link-item">{item.name}</a>
+                  </Link>
+                </li>
+              ))}
+            </div>
           </ul>
-
-      </div>
-
+        </div>
       </div>
 
       <div className="menu-content mobile">
+        <ul className="menu-items">
+          <div className="flex column">
+            {simpleLinks.map((item, i) => (
+              <li className="" key={`${i * 4}`}>
+                <Link href={item.link}>
+                  <a className="link-item">{item.name}</a>
+                </Link>
+              </li>
+            ))}
+          </div>
 
-      <ul className="menu-items">
+          <div className="separeted flex column">
+            {highlightedLinks.map((item, i) => (
+              <li className="" key={`${i * 4}`}>
+                <Link href={item.link}>
+                  <a className="link-item">{item.name}</a>
+                </Link>
+              </li>
+            ))}
+          </div>
 
-        <div className="flex column">
-
-          {simpleLinks.map((item, i) => (
-            <li className="" key={`${i * 4}`}  >
-              <Link href={item.link}>
-                <a className="link-item" >{item.name}</a>
-              </Link>
-            </li>
-          ))}
-
-        </div>
-
-        <div className="separeted flex column">
-
-          {highlightedLinks.map((item, i) => (
-            <li className="" key={`${i * 4}`} >
-              <Link href={item.link}>
-                <a className="link-item">{item.name}</a>
-              </Link>
-            </li>
-          ))}
-
-        </div>
-
-        <div className="logo-container">
-
-          <Image objectFit="contain" width="70px" height="70px" src="/logo.png" alt="Logo" className='logo' />
-
-        </div>
-
+          <div className="logo-container">
+            <Image
+              objectFit="contain"
+              width="70px"
+              height="70px"
+              src="/logo.png"
+              alt="Logo"
+              className="logo"
+            />
+          </div>
         </ul>
-
       </div>
-
     </div>
-   
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
