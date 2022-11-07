85eb792c4bff59c1cb6411025fdc5d0f57dcbc90
unknown <hassanad94@gmail.com>
2022-11-07 12:13:35 +0100

    desktop hidden

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import { StateContext } from "../context/settingContext";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 function MyApp({ Component, pageProps }) {
+  const [desktopRequest, setDesktopRequest] = useState(false);
+
+  useEffect(() => {
+    const currentWindowSize = window.innerWidth;
+    setDesktopRequest(currentWindowSize > 1024);
+  }, []);
+
+  if (desktopRequest) {
+    return (
+      <div className="loader">
+        <div>
+          <Image src="/in-build.svg" width={200} height={200} />
+          <h1>Az oldal épp készítjük. Mobilodon megtekinheted.</h1>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <StateContext>
       <Layout>
