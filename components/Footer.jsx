679a3058045c93d3e75173f0afe679a2eba2b5f4
unknown <hassanad94@gmail.com>
2022-11-09 14:36:56 +0100

    footer 50%

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 import { client } from "/lib/client";
 import { useEffect, useState } from "react";
 import OpenMessageModal from "./OpenMessageModal";
+import { useStateContext } from "../context/settingContext";
 
 const Footer = () => {
   const [socials, setSocials] = useState({});
+  const { currentDevice } = useStateContext();
 
   useEffect(() => {
     client
@@ -15,6 +17,94 @@ const Footer = () => {
   }, []);
 
   const { facebook, instagram, youtube } = socials;
+
+  if (currentDevice === "desktop") {
+    return (
+      <>
+        <div className="section footer-desktop footer flex">
+          <div className="flex general">
+            <div className="logo-container flex">
+              <Image
+                width="100"
+                height="100"
+                src="/logo-dark.png"
+                alt="Logo"
+                className="logo"
+              />
+            </div>
+
+            <div className="footer-menu flex column">
+              <h3> Hegyikecskék</h3>
+              <Link href="/rolunk">Rólunk</Link>
+              <Link href="/velemenyek">Vélemények</Link>
+              <Link href="/galeria">Galéria</Link>
+            </div>
+
+            <div className="footer-menu flex column">
+              <h3> Szolgáltatások</h3>
+              <Link href="/edzestervezes">edzéstervezés</Link>
+              <Link href="/konzultacio">konzultáció</Link>
+              <Link href="/esemenyek">események</Link>
+            </div>
+
+            <div className="footer-menu flex column">
+              <h3> Egyesület</h3>
+              <Link href="/informacio">információ</Link>
+              <Link href="/tagsag">tagság</Link>
+              <Link href="/elonyok">előnyök</Link>
+            </div>
+          </div>
+
+          <div className="flex contacts">
+            <h3> Kapcsolat</h3>
+
+            <div className="quick-contact">
+              <a href="mailto:info@hegyikecskek.hu">
+                &#9993; info@hegyikecskek.hu
+              </a>{" "}
+              <br />
+              &#x1F4DE; <a href="tel:+36301234455">+36 30 123 4455</a>
+            </div>
+
+            <div className="social-media-ref">
+              <a href={facebook}>
+                <Image
+                  objectFit="contain"
+                  width="32px"
+                  height="32px"
+                  src="/facebook.png"
+                  alt="Logo"
+                  className="logo"
+                />
+              </a>
+
+              <a href={instagram}>
+                <Image
+                  objectFit="contain"
+                  width="32px"
+                  height="32px"
+                  src="/instagram.png"
+                  alt="Logo"
+                  className="logo"
+                />
+              </a>
+
+              <a href={youtube}>
+                <Image
+                  objectFit="contain"
+                  width="32px"
+                  height="32px"
+                  src="/youtube.png"
+                  alt="Logo"
+                  className="logo"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 
   return (
     <div className="section footer">
@@ -44,12 +134,11 @@ const Footer = () => {
         <Link href="/elonyok">előnyök</Link>
       </div>
 
-      <OpenMessageModal buttonTitle="Edzéstervet szeretnék!" />
-      <br />
-      <OpenMessageModal buttonTitle="Tag szeretnék lenni!" />
+      <h3> Kapcsolat</h3>
 
       <div className="quick-contact">
-        <a href="mailto:info@hegyikecskek.hu">&#9993; info@hegyikecskek.hu</a> |
+        <a href="mailto:info@hegyikecskek.hu">&#9993; info@hegyikecskek.hu</a>{" "}
+        <br />
         &#x1F4DE; <a href="tel:+36301234455">+36 30 123 4455</a>
       </div>
 
@@ -87,6 +176,10 @@ const Footer = () => {
           />
         </a>
       </div>
+
+      <OpenMessageModal buttonTitle="Edzéstervet szeretnék!" />
+      <br />
+      <OpenMessageModal buttonTitle="Tag szeretnék lenni!" />
     </div>
   );
 };
